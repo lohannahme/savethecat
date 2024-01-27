f40e302992f69eff861bd1d76778e90e652d61d0
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-01-27 13:41:36 -0300

    spawner created

---
+++ savethecat/Assets/Scripts/CircleSpawner.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleSpawner : MonoBehaviour
+{
+    public int NumOfCircles = 1;
+    public int NumOfObjects = 1;
+
+    public float RadiusSize = 15f;
+
+    public GameObject prefab;
+    public Transform spawnerPos;
+
+    List<GameObject> spawnedGo = new List<GameObject>();
+    int NumOfCirclesLast = 0;
+
+    private void Awake()
+    {
+        NumOfCirclesLast = NumOfCircles;
+    }
+
+    private void Start()
+    {
+        SpawnNew();
+    }
+
+    private void Update()
+    {
+        if (NumOfCircles != NumOfCirclesLast)
+            SpawnNew();
+    }
+
+    void SpawnNew()
+    {
+        foreach (var item in spawnedGo)
+        {
+            Destroy(item);
+        }
+
+        Vector2 center = spawnerPos.position;
+
+        for (int i = 1; i <= NumOfCircles; i++)
+        {
+            for (int j = 0; j < NumOfObjects; j++)
+            {
+                
+                int a = 360 / NumOfObjects * j;
+                Vector3 pos = RandomCircle(center, RadiusSize * i, a);
+                GameObject newGo = Instantiate(prefab, pos, Quaternion.identity);
+
+                spawnedGo.Add(newGo);
+            }
+            NumOfCirclesLast = NumOfCircles;
+        }
+    }
+
+    Vector3 RandomCircle(Vector3 center, float radius, int a)
+    {
+        float ang = a;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.z = center.z;
+        return pos;
+    }
+}
