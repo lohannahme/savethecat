e181257c0165afb56eee597c0301f58e4b80462b
Hanna <61636785+lohannahme@users.noreply.github.com>
2024-01-27 14:10:07 -0300

    Player level up

--- savethecat/Assets/Scripts/Player/PlayerXPManager.cs
+++ savethecat/Assets/Scripts/Player/PlayerXPManager.cs
@@ -6,13 +6,33 @@ using System;
 public class PlayerXPManager : MonoBehaviour
 {
     private float _currentXP = 0;
-    private float _xpToNextLevel = 20;
+    private float _xpToNextLevel = 10;
+    private int _playerLevel = 1;
+    private int _xpConst = 10;
+
 
     public static Action<float, float> UpdateExperience;
+    public static Action LevelUp;
+
 
     public void UpdatePlayerExperience(float experience)
     {
         _currentXP += experience;
+
+        if (_currentXP == _xpToNextLevel)
+        {
+            LevelUp?.Invoke();
+            _playerLevel++;
+            UpdatePlayerLevel();
+        }
+
         UpdateExperience?.Invoke(_currentXP, _xpToNextLevel);
+    }
+
+    private void UpdatePlayerLevel()
+    {
+        _currentXP = 0;
+        _xpToNextLevel = _xpConst * _playerLevel;
+        LevelUp?.Invoke();
     }
 }
--- savethecat/Assets/Scripts/UI/GameUI.cs
+++ savethecat/Assets/Scripts/UI/GameUI.cs
@@ -7,18 +7,27 @@ public class GameUI : MonoBehaviour
 {
     [SerializeField] private Image _xpBar;
 
+    [SerializeField] private GameObject _levelUpPanel;
+
     private void OnEnable()
     {
         PlayerXPManager.UpdateExperience += UpdateXPBar;
+        PlayerXPManager.LevelUp += LevelUp;
     }
 
     private void OnDisable()
     {
         PlayerXPManager.UpdateExperience -= UpdateXPBar;
+        PlayerXPManager.LevelUp -= LevelUp;
     }
 
     private void UpdateXPBar(float xpAmount, float totalXP)
     {
         _xpBar.fillAmount = xpAmount / totalXP;
+    }
+
+    private void LevelUp()
+    {
+        Time.timeScale = 0;
     }
 }
