7ab0a3c9dad483e7fca6b688b33458616fcd738e
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-01-27 10:14:36 -0300

    EnemyController created

---
+++ savethecat/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public Transform player;
+    public float EnemySpeed = 2f;
+    public bool isFacingRight = true;
+    public float minDistance = 1f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+          if (player.position.x > rb.position.x)
+        {
+            transform.localScale = new Vector2(1f,transform.localScale.y);
+            isFacingRight = true;
+        }
+        else if (player.position.x < rb.position.x)
+        {
+            transform.localScale = new Vector2(-1f,transform.localScale.y);
+            isFacingRight = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Vector2.Distance(rb.position,player.position) > minDistance)
+        {
+            rb.MovePosition(Vector3.MoveTowards(rb.position,player.position,EnemySpeed * Time.fixedDeltaTime));
+        }
+    }
+}
