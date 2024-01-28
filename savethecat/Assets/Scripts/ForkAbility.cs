2a85ecc37058274afc4dc8d490243334016a5158
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-01-28 13:19:04 -0300

    forks with timer now

--- savethecat/Assets/Scripts/ForkAbility.cs
+++ savethecat/Assets/Scripts/ForkAbility.cs
@@ -9,8 +9,9 @@ public Transform player;
 public float speed;
 public GameObject[] forks;
 public bool hasAbility;
-public int forksActivated;
-public float timer;
+public int forksActivated = 3;
+public float lastEnabledTime = 0;
+public float cooldown = 2;
     void Start()
     {
         
@@ -23,17 +24,26 @@ public float timer;
 
             for(int i=0;i<forks.Length;i++){
                 forks[i].SetActive(true);
-            if(forks[i].activeSelf==true){
-                forksActivated+=1;
+           
+                if(forks[i].activeSelf==true){
+                    forksActivated+=1;
                 }
+
+                Debug.Log("has ability and forks <3");
             }
 
         }
+        
+        if(Time.time > lastEnabledTime + cooldown)
+        {
+            for(int i=0;i<forks.Length;i++){
+            forks[i].SetActive(!forks[i].activeSelf);
+            }
+            lastEnabledTime = Time.time;
+            Debug.Log("acessou o codigo");
+        }
 
         transform.position=player.position;
-        if(transform.eulerAngles.z>=360){
-            timer=0;
-        }    timer+=Time.deltaTime;
 
         transform.eulerAngles=new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,transform.eulerAngles.z-speed);
     
